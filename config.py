5d633383ee69ff5baeed3bb63314aba51a010b85
Alexey Luzin <luzin.a@samberi.com>
2019-12-18 17:39:01 +1000

    чтение файла и внесение переменных в os.environ + закоментил лишнее

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
 from os import environ as envs
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
+import json
+# from dotenv import load_dotenv, dotenv_values
+# load_dotenv()
+
+SETTINGS_FILENAME = 'settings.json'
 
 
-WAREINFO_API_URL = envs['WAREINFO_API_URL']
+# загоняем настройки в систему, если они вообще есть
+with open(SETTINGS_FILENAME, mode='r') as _file:
+    _vars = json.load(_file)
+    for k, v in _vars.items():
+        if type(v) == int:
+            v = str(v)
+        envs[k] = v
+
+
+# WAREINFO_API_URL = envs['WAREINFO_API_URL']
 # SHOP_NUMBER = envs['SHOP_NUMBER']
-SOFTCHEQUE_URL = envs['SOFTCHEQUE_URL']
+# SOFTCHEQUE_URL = envs['SOFTCHEQUE_URL']
 
-SOFTCHEQUE_PRINTER = 'TSC_TDP-225'
+# SOFTCHEQUE_PRINTER = 'TSC_TDP-225'
 
 PRINT_COMMAND = 'lp -d'
 
-print('env values', dotenv_values())
+# print('env values', dotenv_values())
